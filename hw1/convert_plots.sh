5305a74bc22488079bcb81cdc43356dc1c130bcd
Sidoh <chris.mullins10@gmail.com>
2011-09-15 21:39:38 -1000

    Comments, cleaning up.

--- hw1/convert_plots.sh
+++ hw1/convert_plots.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Bash script to convert .png images into .eps using ImageMagick. It also
+## inserts a bit of postscript into the converted EPS files that force the
+## dvi -> pdf distiller to use high quality images. Without this, dvipdf
+## introduces some nasty JPEG artifacts in all rasturized images, which is
+## no good.
+
 PS_TO_INSERT=$(printf '%q', "
 systemdict /setdistillerparams known {
 << /AutoFilterColorImages false /ColorImageFilter /FlateEncode >>  
